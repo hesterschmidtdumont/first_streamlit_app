2bc7fa3b8ab34c3f133a26c269958d0b7fe0b58a
hesterschmidtdumont <149491688+hesterschmidtdumont@users.noreply.github.com>
2023-11-01 16:37:49 +0200

    Update streamlit_app.py
    Update advice to be displayed in a table

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-# New Section to display fruitvice api response
+# New Section to display fruitvice api response.
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Output it on the screen as a table
+streamlit.dataframe(fruityvice_normalized)
